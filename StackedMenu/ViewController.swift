7c4e67ce553bb1908b42f761c2a23dc676432303
Fredrick Ohen <fredrickohen@gmail.com>
2017-03-17 23:32:11 -0500

    Added animator and gravity

--- StackedMenu/ViewController.swift
+++ StackedMenu/ViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  let data = ["Modern Workspace", "West Coast Style Living", "Relaxed Environment"]
+  let data = ["Modern Workspace", "West Coast Style Lifestyle", "Relaxed Environment"]
   
   var views: [UIView] = []
-  var animator: UIDynamicAnimator?
-  var gravity: UIGravityBehavior?
-  var snap: UISnapBehavior?
+  var animator: UIDynamicAnimator!
+  var gravity: UIGravityBehavior!
+  var snap: UISnapBehavior!
   var previousTouchPoint: CGPoint?
+  var viewDragging = false
+  var viewPinned = false
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    addAnimator()
+    createGravity()
+  }
+  
+  
+  func addViewController (atOffset offset: CGFloat, dataForVC: AnyObject?) -> UIView? {
+    return nil
+  }
+  
+  func addAnimator() {
+    animator = UIDynamicAnimator(referenceView: self.view)
+    animator.addBehavior(gravity)
+
+  }
+  
+  func createGravity() {
+    gravity = UIGravityBehavior()
+    gravity.magnitude = 5
   }
 
 }
-
